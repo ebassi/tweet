6b1ca6f34af56aa7ed0430a260fc3ac3ebc6c093
Emmanuele Bassi <ebassi@openedhand.com>
2008-04-10 21:22:00 +0100

    Add test case for the user-timeline command
    This test case retrieves the timeline of the passed user. It
    checks authentication and the user-timeline command.
    
    The test case has a built-in termination sequence so that it
    never runs more than 10 seconds.

---
+++ tests/test-user-timeline.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <twitter-glib/twitter-glib.h>
+
+static GMainLoop *main_loop = NULL;
+
+static gboolean
+authenticate_cb (TwitterClient    *client,
+                 TwitterAuthState  state,
+                 gpointer          user_data)
+{
+  g_print ("Authenticating...\n");
+
+  return TRUE;
+}
+
+static void
+status_received_cb (TwitterClient *client,
+                    TwitterStatus *status,
+                    const GError  *error,
+                    gpointer       user_data)
+{
+  TwitterUser *user;
+
+  if (error)
+    {
+      g_print ("Unable to retrieve the user timeline: %s\n", error->message);
+
+      if (g_main_loop_is_running (main_loop))
+        g_main_loop_quit (main_loop);
+
+      return;
+    }
+
+  g_assert (TWITTER_IS_STATUS (status));
+
+  user = twitter_status_get_user (status);
+  if (!user)
+    return;
+
+  g_print ("<%s> %s\n"
+           "\t-- created at: %s\n",
+           twitter_user_get_name (user),
+           twitter_status_get_text (status),
+           twitter_status_get_created_at (status));
+}
+
+static gboolean
+get_user_timeline (gpointer data)
+{
+  TwitterClient *client = data;
+
+  twitter_client_get_user_timeline (client, NULL, 0, NULL);
+
+  return FALSE;
+}
+
+static gboolean
+timeout_quit (gpointer data)
+{
+  if (g_main_loop_is_running (main_loop))
+    g_main_loop_quit (main_loop);
+
+  return FALSE;
+}
+
+int
+main (int   argc,
+      char *argv[])
+{
+  TwitterClient *client;
+
+  g_type_init ();
+  g_thread_init (NULL);
+
+  if (argc < 3)
+    {
+      g_print ("Usage: test-user-timeline <email> <password>\n");
+      return EXIT_FAILURE;
+    }
+
+  client = twitter_client_new_for_user (argv[1], argv[2]);
+  g_signal_connect (client, "authenticate",
+                    G_CALLBACK (authenticate_cb),
+                    NULL);
+  g_signal_connect (client, "status-received",
+                    G_CALLBACK (status_received_cb),
+                    NULL);
+
+  main_loop = g_main_loop_new (NULL, FALSE);
+
+  g_timeout_add_seconds (1, get_user_timeline, client);
+  g_timeout_add_seconds (10, timeout_quit, NULL);
+
+  g_main_loop_run (main_loop);
+
+  g_object_unref (client);
+  g_main_loop_unref (main_loop);
+
+  return EXIT_SUCCESS;
+}
