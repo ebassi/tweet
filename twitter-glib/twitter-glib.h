9a3d1704859f75941ae0d80f8348e11cf6b1099d
Emmanuele Bassi <ebassi@openedhand.com>
2008-04-10 21:43:12 +0100

    Last bit of the Reader -> Client rename
    The twitter-reader.h header has been renamed into twitter-client.h.

--- twitter-glib/twitter-glib.h
+++ twitter-glib/twitter-glib.h
@@ -1,9 +1,9 @@
 #ifndef __TWITTER_GLIB_H__
 #define __TWITTER_GLIB_H__
 
+#include <twitter-glib/twitter-client.h>
 #include <twitter-glib/twitter-common.h>
 #include <twitter-glib/twitter-enum-types.h>
-#include <twitter-glib/twitter-reader.h>
 #include <twitter-glib/twitter-status.h>
 #include <twitter-glib/twitter-timeline.h>
 #include <twitter-glib/twitter-user.h>
