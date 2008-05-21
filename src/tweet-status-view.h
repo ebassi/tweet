75c5b981d28a703aaafee9204a4882abef4da957
Emmanuele Bassi <ebassi@openedhand.com>
2008-05-21 16:18:55 +0100

    Add licensing notices to all Tweet source files
    Tweet is released under the terms of the GNU General Public License,
    version 3.0 or later.
    
    The LGPL v2.1 (or later) used by GLib, GTK+ and Clutter is compatible
    with the GPL v3, according to the FSF.

--- src/tweet-status-view.h
+++ src/tweet-status-view.h
@@ -1,3 +1,21 @@
+/* tweet-status-view.h: Status list viewer
+ *
+ * This file is part of Tweet.
+ * Copyright (C) 2008  Emmanuele Bassi  <ebassi@gnome.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __TWEET_STATUS_VIEW_H__
 #define __TWEET_STATUS_VIEW_H__
 
--- src/tweet-window.c
+++ src/tweet-window.c
@@ -1,3 +1,21 @@
+/* tweet-window.c: Main application window
+ *
+ * This file is part of Tweet.
+ * Copyright (C) 2008  Emmanuele Bassi  <ebassi@gnome.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
